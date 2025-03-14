c94ad319015bf0e8f95212ac3d77f4ee44710bef
BrandonRogers <ber64111@gmail.com>
2025-03-14 13:15:02 -0600

    Update routing

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Mission",
@@ -13,6 +15,25 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["NASA", "Blue Origin"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
 module.exports = {
-  launches,
+  getAllLaunches,
+  addNewLaunch,
 };
