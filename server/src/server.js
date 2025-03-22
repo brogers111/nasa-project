bc5aa03b39ede0ad79856809ab5217fae35d033f
BrandonRogers <ber64111@gmail.com>
2025-03-22 14:48:04 -0600

    Add mongoDB and schema

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,23 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://ber64111:Ch0fuRic3ball@nasacluster.bjcz4.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
